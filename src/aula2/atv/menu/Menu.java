9330e6718c3d1b92f4ee4ffe6e68b5b49784ff44
leonardo.goandete <leonardo.goandete>
2023-03-15 13:58:39 -0300

    Realizado implementacao do cadastro de Conta Especial e Menu usando ENUM.

--- src/aula2/atv/menu/Menu.java
+++ src/aula2/atv/menu/Menu.java
@@ -3,27 +3,25 @@ package aula2.atv.menu;
 import aula2.atv.agencias.Agencia;
 import aula2.atv.clientes.Cliente;
 import aula2.atv.contas.ContaEspecial;
+import aula2.utils.OpcoesMenu;
 
 import javax.swing.*;
 import java.util.ArrayList;
-import java.util.List;
 
 public class Menu {
 
     public static int exibeMenu(){
-        return Integer.parseInt(JOptionPane.showInputDialog(
-                        "1 – Cadastrar Conta Especial \n" +
-                        "2 – Pesquisar Conta Especial usando número\n" +
-                        "3 - Pesquisar Conta Especial usando número da agência\n" +
-                        "4 – Pesquisar Conta Especial usando o CPF do cliente \n" +
-                        "5 - Listar todos os clientes \n" +
-                        "6 - Sair \n" +
-                        "Digite uma opção: " ));
+        String string_menu = "";
+        for(OpcoesMenu opcaoMenu : OpcoesMenu.values())
+            string_menu += opcaoMenu.toString();
+        return Integer.parseInt(JOptionPane.showInputDialog(null,string_menu));
+
     }
     public static void main(String[] args) {
 
-        do {
-            switch (exibeMenu()){
+        //do {
+        while (true) {
+            switch (exibeMenu()) {
                 case 1:
                     ArrayList<Cliente> clientes = new ArrayList<Cliente>();
 
@@ -31,23 +29,28 @@ public class Menu {
                         clientes.add(new Cliente(
                                 JOptionPane.showInputDialog(null, "Digite o nome do cliente:"),
                                 JOptionPane.showInputDialog(null, "Digite o CPF do cliente:")));
-                    }while (JOptionPane.showConfirmDialog(null, "Deseja incluir outro cliente?") == 0);
+                    } while (JOptionPane.showConfirmDialog(null, "Deseja incluir outro cliente?") == 0);
 
                     ContaEspecial ce = new ContaEspecial(
                             new Agencia(Integer.parseInt(
-                                    JOptionPane.showInputDialog(null,"Digite o numero da agencia:")),
-                                    JOptionPane.showInputDialog(null,"Digite o endereco da agencia:")),
+                                    JOptionPane.showInputDialog(null, "Digite o numero da agencia:")),
+                                    JOptionPane.showInputDialog(null, "Digite o endereco da agencia:")),
                             Double.parseDouble(JOptionPane.showInputDialog(null, "Digite o saldo:")),
                             Long.parseLong(JOptionPane.showInputDialog(null, "Digite o numero da conta:")),
                             Double.parseDouble(JOptionPane.showInputDialog(null, "Digite o limite:")),
                             clientes
-                            );
-
-                    System.out.println(ce);
+                    );
+                    break;
+                case 2:
+                    System.out.println("teste");
+                    break;
+                case 6:
+                    System.exit(0);
                     break;
 
 
             }
-        }while (exibeMenu() != 6);
+            //}while (exibeMenu() != 6);
+        }
     }
 }
---
+++ src/aula2/utils/OpcoesMenu.java
@@ -0,0 +1,33 @@
+package aula2.utils;
+
+public enum OpcoesMenu {
+
+    CADASTRAR(1,"Cadastrar Conta Especial"),
+    PESQUI_CE_NUM(2,"Pesquisar Conta Especial usando número"),
+    PESQUI_CE_NUM_AG(3,"Pesquisar Conta Especial usando número da agência"),
+    PESQUI_CE_CPF(4,"Pesquisar Conta Especial usando o CPF do cliente"),
+    LISTA_TODOS(5,"Listar todos os clientes"),
+    SAIR(6,"Sair");
+
+
+    private final int numOpcao;
+    private final String descOpcao;
+
+    private OpcoesMenu(int numOpcao, String descOpcao) {
+        this.numOpcao = numOpcao;
+        this.descOpcao = descOpcao;
+    }
+
+    public int getNumOpcao() {
+        return numOpcao;
+    }
+
+    public String getDescOpcao() {
+        return descOpcao;
+    }
+
+    @Override
+    public String toString() {
+        return numOpcao + " - " + descOpcao+"\n";
+    }
+}
